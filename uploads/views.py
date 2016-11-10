01dce91ef56b400df54ce0930adf990b8c36e067
Michael Lissner <mlissner@michaeljaylissner.com>
2016-11-10 10:11:10 -0800

    Cleanup to views file and tweaks to the view used to identify changed documents to make it more versatile for CourtListener.

--- uploads/views.py
+++ uploads/views.py
@@ -1,9 +1,9 @@
-
 import re
 import logging
 import csv
 
-from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError
+from django.http import HttpResponse, HttpResponseForbidden, \
+    HttpResponseServerError
 from django.utils import simplejson
 
 from uploads.models import Document, Uploader, PickledPut
@@ -109,7 +109,6 @@ def query(request):
                                            "timestamp": <last time seen> },
                             <pacer url>: ... }
     """
-
     response = {}
 
     if request.method != "POST":
@@ -148,9 +147,8 @@ def query(request):
         return HttpResponse(message)
 
     for url in urls:
-
         # detect show_doc style document links
-        sdre = re.search("show_doc\.pl\?(.*)",url)
+        sdre = re.search("show_doc\.pl\?(.*)", url)
 
         if sdre:
             argsstring = sdre.group(1)
@@ -163,7 +161,6 @@ def query(request):
 
             # maybe need to add some checks for whether
             # these vars exist in argsdict
-
             query = Document.objects.filter(court=court) \
                 .filter(docnum=argsdict["doc_num"]) \
                 .filter(casenum=argsdict["caseid"]) \
@@ -176,7 +173,6 @@ def query(request):
             query = Document.objects.filter(docid=docid) \
                 .filter(available=1)
 
-
         if query:
             query = query[0]
             real_casenum = query.casenum
@@ -188,14 +184,12 @@ def query(request):
                                                  query.subdocnum),
                 "timestamp": query.lastdate.strftime("%m/%d/%y")}
 
-
             if query.subdocnum == 0:
-
-                subquery = Document.objects.filter(court=court,
-                                                   casenum=query.casenum,
-                                                   docnum=query.docnum,
-                                                   available=1).exclude(
-                                                   subdocnum=0)
+                subquery = Document.objects.filter(
+                    court=court,
+                    casenum=query.casenum,
+                    docnum=query.docnum,
+                    available=1).exclude(subdocnum=0)
 
                 if len(subquery) > 0:
                     response[url]["subDocuments"] = {}
@@ -203,16 +197,16 @@ def query(request):
                     for subDoc in subquery:
                         real_sub_casenum = subDoc.casenum
                         response[url]["subDocuments"][subDoc.subdocnum] = {
-                                     "filename" : IACommon.get_pdf_url(court,
-                                                              real_sub_casenum,
-                                                              subDoc.docnum,
-                                                              subDoc.subdocnum),
-                                     "timestamp": subDoc.lastdate.strftime("%m/%d/%y")}
-
+                            "filename": IACommon.get_pdf_url(court,
+                                                             real_sub_casenum,
+                                                             subDoc.docnum,
+                                                             subDoc.subdocnum),
+                            "timestamp": subDoc.lastdate.strftime("%m/%d/%y")}
 
     jsonout = simplejson.dumps(response)
 
     return HttpResponse(jsonout, mimetype="application/json")
+
 
 def query_cases(request):
     """  Query the database for the url of the html docket, if it exists
@@ -224,7 +218,6 @@ def query_cases(request):
                            {"docket_url": <public url>,
                             "timestamp": <last time seen> }
     """
-
     response = {}
 
     if request.method != "POST":
@@ -261,14 +254,15 @@ def query_cases(request):
         logging.warning(message)
         return HttpResponse(message)
 
-    doc_query = Document.objects.filter(court=court) \
-                  .filter(casenum=casenum) \
-                  .order_by('-lastdate', '-modified')
+    doc_query = (Document.objects
+                         .filter(court=court)
+                         .filter(casenum=casenum)
+                         .order_by('-lastdate', '-modified'))
 
     yesterday = datetime.datetime.now() - datetime.timedelta(1)
 
-    old_or_avail_query = doc_query.filter(available=1) \
-                         | doc_query.filter(modified__lte=yesterday)
+    old_or_avail_query = doc_query.filter(available=1) | \
+                         doc_query.filter(modified__lte=yesterday)
     query = None
     try:
         query = old_or_avail_query[0]
@@ -278,11 +272,10 @@ def query_cases(request):
         except IndexError:
             query = None
         else:
-            ppquery = PickledPut.objects.filter(filename=IACommon.get_docketxml_name(court, casenum))
+            ppquery = PickledPut.objects.filter(
+                filename=IACommon.get_docketxml_name(court, casenum))
             if len(ppquery) > 0:
                 query = None
-
-
 
     if query:
         try:
@@ -295,15 +288,12 @@ def query_cases(request):
                 date = "Unknown"
 
         response = {
-                      "docket_url": IACommon.get_dockethtml_url(court,
-                                                 casenum),
-                      "timestamp": date}
-
+            "docket_url": IACommon.get_dockethtml_url(court,
+                                                      casenum),
+            "timestamp": date}
 
     jsonout = simplejson.dumps(response)
     return HttpResponse(jsonout, mimetype="application/json")
-                    #No documents exist, therefore no dockets exist
-
 
 
 def adddocmeta(request):
@@ -339,20 +329,21 @@ def adddocmeta(request):
     except KeyError:
         add_case_info = None
 
-
     DocumentManager.handle_adddocmeta(docid, court, casenum, de_seq_num,
                                       dm_id, docnum, subdocnum)
     if add_case_info:
-        response = {"documents": UploadHandler._get_documents_dict(court, casenum),
-                  "message": "adddocmeta: DB updated for docid=%s" % (docid) }
+        response = {
+            "documents": UploadHandler._get_documents_dict(court, casenum),
+            "message": "adddocmeta: DB updated for docid=%s" % (docid)
+        }
         message = simplejson.dumps(response)
     else:
         message = "adddocmeta: DB updated for docid=%s" % (docid)
 
     return HttpResponse(message)
 
-def lock(request):
 
+def lock(request):
     try:
         key = request.GET["key"].strip()
         court = request.GET["court"].strip()
@@ -375,7 +366,8 @@ def lock(request):
 
     # Try to grab the lock.
     lock_nonce, errmsg = BucketLockManager.get_lock(court, casenum,
-                                                    uploaderid, one_per_uploader)
+                                                    uploaderid,
+                                                    one_per_uploader)
 
     if not lock_nonce or lock_nonce == 'bigdoc':
         return HttpResponse("0<br>%s" % errmsg)
@@ -418,7 +410,6 @@ def unlock(request):
 
 
 def querylocks(request):
-
     try:
         key = request.GET["key"].strip()
     except KeyError:
@@ -440,38 +431,38 @@ def querylocks(request):
 
     pairs = ""
     numlocks = 0
-
     for (court, casenum, nonce) in lock_triples:
         pairs += "%s,%s,%s<br>" % (court, casenum, nonce)
         numlocks += 1
 
     return HttpResponse("%d<br>%s" % (numlocks, pairs))
 
+
 def get_updated_cases(request):
     """
-    This view is used by archive.recapthelaw.org to determine what dockets to download from IA
+    This view is used by CourtListener to determine what to download from IA
     """
-    API_KEYS = (u'',)
+    api_keys = (u'',)
 
     params = request.POST
     tpq = params.get('tpq', '')
     api_key = params.get('key', '')
-    if not tpq or api_key not in API_KEYS:
+    if not tpq or api_key not in api_keys:
         return HttpResponseForbidden()
 
     response = HttpResponse(mimetype='text/csv')
     tpq = float(tpq)
     tpq = datetime.datetime.fromtimestamp(tpq)
-    docs = Document.objects.filter(lastdate__gt=tpq) | Document.objects.filter(modified__gt=tpq)
-    urls = set()
-    docs = docs.values('court', 'casenum')
+    docs = Document.objects.filter(lastdate__gt=tpq) | \
+           Document.objects.filter(modified__gt=tpq)
+    columns = ['court', 'casenum', 'docnum', 'subdocnum', 'available']
+    docs = (docs.values(*columns)
+            .order_by('court', 'casenum', 'docnum', 'subdocnum'))
+    w = csv.DictWriter(response, fieldnames=columns)
     for doc in docs:
-       urls.add((doc['court'], doc['casenum']))
-
-    w = csv.writer(response)
-    for url in urls:
-        w.writerow(url)
+        w.writerow(doc)
     return response
+
 
 def heartbeat(request):
     """
@@ -487,12 +478,11 @@ def heartbeat(request):
         return HttpResponseForbidden("403 Forbidden")
 
     query = Document.objects.filter(court='cand', casenum='215270')
-
-    if query.count() >0:
+    if query.count() > 0:
         return HttpResponse("It's Alive!")
     else:
         return HttpResponseServerError("500 Server error: He's Dead Jim")
 
+
 def load():
     return HttpResponse(os.getloadavg().pop())
-
